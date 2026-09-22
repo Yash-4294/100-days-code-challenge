debc0a8b4ffbaf0825bf29083124d89a4181bdd6
Yash-4294 <Yash.42948@stu.upes.ac.in>
2026-09-22 11:48:55 +0530

    Add area and perimeter calculation for rectangle

---
+++ 3.c
@@ -0,0 +1,8 @@
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+
+print(f"Area: {area}")
+print(f"Perimeter: {perimeter}")
