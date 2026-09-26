bb142238e6b0e9fac6ec27fc9c9bab022770dbd8
Yash-4294 <Yash.42948@stu.upes.ac.in>
2026-09-26 11:08:27 +0530

    Modify program to check for leap year

---
+++ 13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    if (scanf("%d", &year) != 1) return 0;
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        printf("Leap year\n");
+    } else {
+        printf("Not a leap year\n");
+    }
+    return 0;
+}
