f3b09f85f2a55d653e991000dd200b469b01d541
Yash-4294 <Yash.42948@stu.upes.ac.in>
2026-09-26 11:02:06 +0530

    Implement simple and compound interest calculation

---
+++ 9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double principal, rate, time;
+    if (scanf("%lf %lf %lf", &principal, &rate, &time) != 3) return 0;
+
+    double si = (principal * rate * time) / 100.0;
+    double ci = principal * (pow(1.0 + (rate / 100.0), time)) - principal;
+    printf("Simple Interest=%.2g, Compound Interest=%.2g\n", si, ci);
+    return 0;
+}
